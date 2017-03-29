9abb0b493d350961d9e414ed05d13ceeda8d06e4
dani <dmarsanz94@gmail.com>
2017-03-29 20:17:53 +0200

    perfilFragment 0.1

--- app/src/main/java/com/danielmartinez/task2danielmartinez/fragments/PerfilFragment.java
+++ app/src/main/java/com/danielmartinez/task2danielmartinez/fragments/PerfilFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.danielmartinez.task2danielmartinez.R;
 
@@ -12,9 +13,12 @@ import com.danielmartinez.task2danielmartinez.R;
 public class PerfilFragment extends Fragment {
 
 
+    private TextView txtNombre, txtApellido, txtNickName, txtEmail;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        txtNickName = (TextView) findViewById(R.id.editNickname);
 
         return inflater.inflate(R.layout.fragment_perfil, container, false);
     }
